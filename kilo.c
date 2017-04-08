d8bf8d24c4de619b37a1ef56e39f92179165a387
ThorodanBrom <bpr_95@hotmail.com>
2017-04-09 02:07:06 +0530

    Disable raw mode at end-of-program

--- kilo.c
+++ kilo.c
@@ -1,15 +1,28 @@
 #include<termios.h>
 #include<unistd.h>
+#include<stdlib.h>
+struct termios org_termios;
+
+void disableRAWMode()
+{
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&org_termios);
+}
 void enableRAWMode()
 {
     /* writing the terminal attribs to the structure -> tcgetattr
      turn off echo mode
      save attribs -> tcsetattr */
-    struct termios raw;
-    tcgetattr(STDIN_FILENO,&raw);
+
+    tcgetattr(STDIN_FILENO,&org_termios);
+    //when program over, run disableraw() to go back to cooked
+    atexit(disableRAWMode);
+    struct termios raw=org_termios;
+
     /* ECHO=0010 -> then NOT is, and bitwise AND to the c_lflag
        echo mode off!!*/
+
     raw.c_lflag &= ~(ECHO);
+
     //tscaflush -> when to apply the changes
     tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
 
