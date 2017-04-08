ce00a413ac04402dede47f11b0e7e668b0e948f4
ThorodanBrom <bpr_95@hotmail.com>
2017-04-09 01:47:46 +0530

    Disabled echo mode(like sudo password)

--- kilo.c
+++ kilo.c
@@ -1,8 +1,24 @@
+#include<termios.h>
 #include<unistd.h>
+void enableRAWMode()
+{
+    /* writing the terminal attribs to the structure -> tcgetattr
+     turn off echo mode
+     save attribs -> tcsetattr */
+    struct termios raw;
+    tcgetattr(STDIN_FILENO,&raw);
+    /* ECHO=0010 -> then NOT is, and bitwise AND to the c_lflag
+       echo mode off!!*/
+    raw.c_lflag &= ~(ECHO);
+    //tscaflush -> when to apply the changes
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+
+}
 int main()
 {
-   char c;
-   //read 1 byte from standard i/p to c
-   while(read(STDIN_FILENO,&c,1)==1);
-   return 0;
+    enableRAWMode();
+    char c;
+    //read 1 byte from standard i/p to c
+    while(read(STDIN_FILENO,&c,1)==1 && c!='q');
+    return 0;
 }
